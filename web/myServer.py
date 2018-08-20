3ca05f109fee19703997914b8e852fbcb7210e22
lost.Fox <lost.fox2012+nuc@gmail.com>
2018-08-20 23:38:13 +0800

    no use pi

---
+++ web/myServer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" 
+@author:james
+@licence: BSD
+@file: myServer.py 
+@time: 2018/08/20
+@contact: lost.Fox2012+py@gmail.com
+@site:  
+@software: PyCharm 
+
+"""  
