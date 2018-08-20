4798e1733cfaca65d64c66acc4137e39544b3d09
lost.Fox <lost.fox2012+nuc@gmail.com>
2018-08-21 00:21:26 +0800

    use basehttpserver

--- web/myServer.py
+++ web/myServer.py
@@ -11,24 +11,31 @@
 
 """
 
-import web
+import BaseHTTPServer
 
-urls = (
-    '/(.*)', 'hello'
-)
+class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    '''处理请求并返回页面'''
 
-app = web.application(urls, globals())
+    # 页面模板
+    Page = '''\
+        <html>
+        <body>
+        <p>Hello, web!</p>
+        </body>
+        </html>
+    '''
 
+    # 处理一个GET请求
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/html")
+        self.send_header("Content-Length", str(len(self.Page)))
+        self.end_headers()
+        self.wfile.write(self.Page)
 
-class hello:
-    def GET(self, name):
-        i = web.input(times=1)
-        if not name:
-            name = 'world'
-        for c in xrange(int(i.times)):
-            print 'Hello,', name + '!'
-        return 'Hello, ' + name + '!'
+#----------------------------------------------------------------------
 
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    serverAddress = ('', 4242)
+    server = BaseHTTPServer.HTTPServer(serverAddress, RequestHandler)
+    server.serve_forever()
