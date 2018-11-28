9f6b5aedc43cddb0ddc45717b71b984df42cd52c
lost.Fox <lost.fox2012+nuc@gmail.com>
2018-11-28 21:20:32 +0800

    using python3

---
+++ basics/testTurtle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" 
+@author:james
+@licence: BSD
+@file: testTurtle.py 
+@time: 2018/11/28
+@contact: lost.Fox2012+py@gmail.com
+@site:  
+@software: PyCharm 
+
+"""
+
+import turtle as tt
+
+tt.setup()
+tt.forward(100)
