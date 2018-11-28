2031487d71c40aab054d1fda763a1a7dc6012ee5
lost.Fox L <lost.Fox2012+sT@gmail.com>
2018-11-28 23:39:22 +0800

    fake 3d

--- basics/testTurtle.py
+++ basics/testTurtle.py
@@ -13,5 +13,22 @@
 
 import turtle as tt
 
-tt.setup()
-tt.forward(100)
+tt.TurtleScreen._RUNNING = True  # 启动绘图，在IDE中运行加这句可避免报错
+
+tt.bgcolor("black")  # 背景色为黑色
+tt.colormode(255)  # 颜色模式为真彩色
+tt.pencolor("white")  # 画笔颜色每次随机
+
+#cnt = 0
+#while cnt < 5:
+#    tt.forward(200)
+#    tt.right(144)
+#    cnt += 1
+
+for i in range(0, 6):
+    for j in range(0, 6):
+        tt.forward(100)
+        tt.right(60)
+    tt.right(60)
+
+tt.done()  # 结束绘图，这将不会关闭窗口
