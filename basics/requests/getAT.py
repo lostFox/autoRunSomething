b88c28d75633574595a43214c480612f536ebb37
lostFox <lost.Fox2012+w10@gmail.com>
2018-12-27 08:38:51 +0800

    fix python3

--- basics/requests/getAT.py
+++ basics/requests/getAT.py
@@ -5,9 +5,9 @@ __author__ = 'james'
 import requests, json
 
 def getAT():
-    # r = requests.post('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx22571d9ee91f8c0e&secret=ed215bb891e434b103ca53dd45dc9eb0')
-    r = requests.post(
-        'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx794f2868ad431638&secret=047e0c5a5830652051f42c588d3f5648')
+    r = requests.post('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx22571d9ee91f8c0e&secret=ed215bb891e434b103ca53dd45dc9eb0')
+    # r = requests.post(
+    #     'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx794f2868ad431638&secret=047e0c5a5830652051f42c588d3f5648')
 
     access_token = ''
 
@@ -18,5 +18,5 @@ def getAT():
     return access_token
 
 if __name__ == '__main__':
-    print 'get access token..'
-    print getAT()
+    print('get access token..')
+    print(getAT())
