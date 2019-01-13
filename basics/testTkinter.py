848d2f76f627ec4ed2e2cc228f68dce5228a56c6
lostFox <lost.fox2012+nuc@gmail.com>
2019-01-13 19:45:29 +0800

    use local txt, test tkinter

---
+++ basics/testTkinter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" 
+@author:james
+@licence: BSD
+@file: testTkinter.py 
+@time: 2019/01/06
+@contact: lost.Fox2012+py@gmail.com
+@site:  
+@software: PyCharm 
+
+"""
+
+import tkinter as tk
+
+window = tk.Tk()  # 生成窗口对象
+window.geometry("500x300")  # 设置窗口大小
+window.title("简单图形界面程序")  # 设置窗口标题
+
+# 这里可以生成其他部件并放入窗口
+
+tk.mainloop()  # 开始主循环
+
+
