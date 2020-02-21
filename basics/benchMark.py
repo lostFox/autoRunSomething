10f8be39ea2fc72189aed08cdfa7c056f5da59d9
lostFox <lost.fox2012+nuc@gmail.com>
2020-02-21 22:26:17 +0800

    using python3

---
+++ basics/benchMark.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" 
+@author:james
+@licence: BSD
+@file: benchMark.py 
+@time: 2020/02/21
+@contact: lost.Fox2012+py@gmail.com
+@site:  
+@software: PyCharm 
+
+"""
+import test.pystone
+print test.pystone.pystones()
