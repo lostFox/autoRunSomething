a86f6b28f9a68e709fa5bccde7457ffae1db8b54
zhanglei <lei_zhang@jit.com.cn>
2020-03-18 13:41:52 +0800

    tls client use ssl.wrap_socket

---
+++ basics/requests/sslClient.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import socket, ssl, pprint, time
+
+HOST = '127.0.0.1'
+PORT = 443
+BUFSIZE = 1024
+ADDR = (HOST, PORT)
+
+# socket create success
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# require a certificate from the server
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+ssl_sock = ssl.wrap_socket(s, ca_certs='./rootCA.pem', cert_reqs=ssl.CERT_REQUIRED)
+# socket connect success
+try:
+    ssl_sock.connect(ADDR)
+
+# note that closing the SSLSocket will also close the underlying socket
+    pprint.pprint(ssl_sock.getpeercert())
+    ssl_sock.send(b'GET / HTTP/1.1\r\n111\r\nConnection: close\r\n\r\n')
+    msg = ssl_sock.recv(1024).decode("utf-8")
+    print("receive msg from server : {%1}", msg)
+# while True:
+#     data = raw_input('> ')
+#     if not data:
+#         break
+#     ssl_sock.send(data)
+#     data = ssl_sock.recv(BUFSIZE)
+#     if not data:
+#         break
+#     print data
+finally:
+    ssl_sock.close()
